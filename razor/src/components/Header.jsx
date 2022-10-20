c6950415077e702d1d599db5a0c39825f707b0eb
Caio Henrique <caiohenriquedev@gmail.com>
2022-10-19 23:34:04 -0300

    terminando header do site

---
+++ razor/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logorazor from "./img/logorazorb.png";
+import "./style.css";
+
+function header() {
+  return (
+    <>
+      <header className="header-razor">
+        <img className="img-logorazor" src={logorazor} alt="logo razor" />
+        <nav>
+          <ul className="list-nav">
+            <li className="navbar-call">
+              <a href="https://razor.com.br" target="_blank"
+                className="link-navbar"
+                rel="noreferrer">contato</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default header;
