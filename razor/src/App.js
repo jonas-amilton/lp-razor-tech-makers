2dfbf5217d2885ad81f8b2928860ed14172b3f4a
Jonas Silva <jonas_profissional@outlool.com>
2022-10-21 20:41:16 -0300

    iniciei a area da ficha tecnica

--- razor/src/App.js
+++ razor/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from './components/Header.jsx'
+import Principal from '.components/Principal';
+import FichaTecnica from "./components/FichaTecnica";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Principal/>
+      <FichaTecnica/>
       <main>
       
 
---
+++ razor/src/components/FichaTecnica.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FichaTecnica.css";
+
+function fichatecnica() {
+  return (
+    <>
+      <img src="./img/fichatec.png" alt="ficha tecnica" />
+    </>
+  );
+}
+
+export default fichatecnica;
