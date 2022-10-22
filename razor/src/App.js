dcb7d2370bd266ac1bf735e71f5f799bff07da8d
Jonas Silva <jonas_profissional@outlool.com>
2022-10-21 22:36:13 -0300

    ficha tecnica

--- razor/src/App.js
+++ razor/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 import Header from './components/Header.jsx';
 import Principal from './components/Principal.jsx';
+import FichaTecnica from './components/FichaTecnica.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header/> 
       <Principal/>
+      
       <main>
-       
+       <FichaTecnica/>
         
       </main>
     </div>
