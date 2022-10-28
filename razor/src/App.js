66ab31151e43882a672719d12ed25930d5aeca48
Jonas Silva <jonas_profissional@outlool.com>
2022-10-28 18:33:59 -0300

    iniciei o footer

--- razor/src/App.js
+++ razor/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Routes from './routes';
 import NossaEquipe from './components/squad.jsx';
+import Footer from './pages/Home/Footer';
 
 function App() {
   return (
     <>
     <Routes />
     <NossaEquipe />
+    <Footer />
     </>
   );
 }
---
+++ razor/src/pages/Home/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <h2>teste</h2>
+    </>
+  );
+}
+export default Footer;
