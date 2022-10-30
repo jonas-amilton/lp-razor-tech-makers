ac9ae872962d0a5ad78403eabc30d4ff00ee0f65
Erik <erik.esperidiao2001@gmail.com>
2022-10-30 15:14:27 -0300

    att footer

--- razor/src/components/Footer.js
+++ razor/src/components/Footer.js
@@ -9,7 +9,6 @@ Heading,
 } from "./FooterStyles";
 import { FaFacebookSquare } from "react-icons/fa"
 import {FaLinkedin} from "react-icons/fa"
-import {FaTwitterSquare} from "react-icons/fa"
 import {FaInstagram} from "react-icons/fa"
 import {FaWhatsapp} from "react-icons/fa"
 import {FaYoutubeSquare} from "react-icons/fa"
@@ -28,47 +27,48 @@ return (
 		</Column>	
 		<Column>
 			<Heading>Razor do Brasil LTDA</Heading>
-			<FooterLink href="https://www.google.com/maps/place/R.+Dr.+Pl%C3%ADnio+Moura,+153+-+Vila+Planaltina,+Passo+Fundo+-+RS,+99062-340/@-28.2822519,-52.3771016,17z/data=!3m1!4b1!4m5!3m4!1s0x94e2bfcf2d8591c3:0x78b2d63164e6eb5e!8m2!3d-28.2822519!4d-52.3771016">Rua Dr. Plínio Moura, 153C</FooterLink>
-			<FooterLink href="#">Vila Planaltina - 99062-340</FooterLink>
-			<FooterLink href="#">Passo Fundo/RS</FooterLink>
+			<FooterLink href="https://goo.gl/maps/YsAsMTdo9VmqwXjn9" target="_blank">Rua Dr. Plínio Moura, 153C</FooterLink>
+			<FooterLink href="https://goo.gl/maps/YsAsMTdo9VmqwXjn9" target="_blank">Vila Planaltina - 99062-340</FooterLink>
+			<FooterLink href="https://goo.gl/maps/YsAsMTdo9VmqwXjn9" target="_blank">Passo Fundo/RS</FooterLink>
 		</Column>
 		<Column>
 			<Heading></Heading>
-			<FooterLink href="#">
+			<FooterLink href="https://www.facebook.com/razorcomp/" target="_blank">
 			<i className="fab fa-facebook-f">
 				<span style={{ marginLeft: "10px" }}>
 				<FaFacebookSquare />
 				</span>
 			</i>
 			</FooterLink>
-			<FooterLink href="#">
+			<FooterLink href="https://www.instagram.com/razor_computadores/" target="_blank">
 			<i className="fab fa-instagram">
 				<span style={{ marginLeft: "10px" }}>
 				<FaInstagram />
 				</span>
 			</i>
 			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-twitter">
-				<span style={{ marginLeft: "10px" }}>
-				<FaTwitterSquare />
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
+			<FooterLink href="https://www.youtube.com/channel/UCinRMTaJQqqqqshaoYkuLoA" target="_blank">
 			<i className="fab fa-youtube">
 				<span style={{ marginLeft: "10px" }}>
 				<FaYoutubeSquare />
 				</span>
 			</i>
 			</FooterLink>
-			<FooterLink href="#">
+			<FooterLink href="https://api.whatsapp.com/send?phone=555430466350&text=Ol%C3%A1.%20Estou%20no%20site%20da%20Razor%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es." target="_blank">
 			<i className="fab fa-youtube">
 				<span style={{ marginLeft: "10px" }}>
 				<FaWhatsapp />
 				</span>
 			</i>
 			</FooterLink>
+			<FooterLink href="https://br.linkedin.com/company/razor-computadores-do-brasil" target="_blank">
+			<i className="fab fa-youtube">
+				<span style={{ marginLeft: "10px" }}>
+				<FaLinkedin />
+				</span>
+			</i>
+			</FooterLink>
+
 			
 		</Column>
 		</Row>
