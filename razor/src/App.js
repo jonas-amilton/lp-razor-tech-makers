c3ffca5ae2435266a365740a9223045f40d7cb6d
Jonas Silva <jonas_profissional@outlool.com>
2022-10-29 21:45:02 -0300

    footer implantado

--- razor/src/App.js
+++ razor/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Routes from './routes';
 import NossaEquipe from './components/squad.jsx';
-import Footer from './components/Footer.jsx';
 import Form from './components/Form.jsx';
 import { Fragment } from 'react';
 import ScrollButton from './components/ScrollButton.js';
-import { Content, Heading } from './components/Styles';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
