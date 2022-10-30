d9c5ded79ca9cf0e4f2113a2d9a55fb2aa537749
Jonas Silva <jonas_profissional@outlool.com>
2022-10-29 21:34:15 -0300

    coloquei o botao de voltar para o topo

--- razor/src/App.js
+++ razor/src/App.js
@@ -3,14 +3,20 @@ import Routes from './routes';
 import NossaEquipe from './components/squad.jsx';
 import Footer from './components/Footer.jsx';
 import Form from './components/Form.jsx';
+import { Fragment } from 'react';
+import ScrollButton from './components/ScrollButton.js';
+import { Content, Heading } from './components/Styles';
 
 function App() {
   return (
     <>
+    <Fragment>
     <Routes />
     <Form />
     <NossaEquipe />
     <Footer />
+      <ScrollButton />
+    </Fragment>
     </>
   );
 }
