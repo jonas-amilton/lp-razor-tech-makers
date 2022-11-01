24a3ba273b8ea65ef6a9dfd5d3c79004a07cfb30
Caio Henrique <caiohenriquedev@gmail.com>
2022-11-01 13:06:33 -0300

    resolvendo scrollBotton

--- razor/src/components/StylesScrollButton.js
+++ razor/src/components/StylesScrollButton.js
@@ -2,12 +2,15 @@ import styled from 'styled-components';
   
 export const Button = styled.div`
    position: fixed; 
-   width: 100%;
-   left: 70%;
+   width: 90%;
+   left: 90%;
    bottom: 100px;
    height: 80px;
    font-size: 3.5rem;
    z-index: 1;
    cursor: pointer;
    color: #8872E7;
+   @media (max-width:768px){
+      left: 87%;
+   }
 `
